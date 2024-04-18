be6253f4e1c51524b0fe997c163025e94b846cd5
exflikt <qff3P.m8d1ue@protonmail.com>
2024-04-18 13:37:47 +0900

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// https://github.com/TomDoesTech/GOTTH/blob/main/tailwind.config.js
+// const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'components/**/*.templ',
+  ],
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       primary: colors.blue,
+  //       secondary: colors.yellow,
+  //       neutral: colors.gray,
+  //     }
+  //   },
+  // },
+  // plugins: [
+  //   require('@tailwindcss/forms'),
+  //   require('@tailwindcss/typography'),
+  // ]
+}
