cd1a65e41836c0661740888ef699e4f2455cd9b3
exflikt <qff3P.m8d1ue@protonmail.com>
2024-07-20 23:34:28 +0900

    Add several path operations
    This commit comes with these architectural changes:
    - New order session template to refresh order items every time there is
      a modification to it
    - Interface to deal with database

--- app/db.py
+++ app/db.py
@@ -1,18 +1,41 @@
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
 import sqlalchemy
 from databases import Database
+from pydantic import BaseModel
+
+DATABASE_URL = "sqlite:///app.db"
+db = Database(DATABASE_URL)
 
 
-@dataclass
-class Product:
+class Table(ABC):
+    TABLE: sqlalchemy.Table
+
+    @classmethod
+    @abstractmethod
+    async def ainit(cls) -> None:
+        pass
+
+    @classmethod
+    async def create_if_not_exists(cls) -> None:
+        schema = sqlalchemy.schema.CreateTable(cls.TABLE, if_not_exists=True)
+        await db.execute(str(schema.compile()))
+
+    @classmethod
+    async def empty(cls) -> bool:
+        return await db.fetch_one(cls.TABLE.select()) is None
+
+
+# TODO: add price field
+# TODO: add no. stock field
+class Product(BaseModel):
     product_id: int
     name: str
     filename: str
 
 
-class ProductTable:
-    _table = sqlalchemy.Table(
+class ProductTable(Table):
+    TABLE = sqlalchemy.Table(
         "products",
         sqlalchemy.MetaData(),
         sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
@@ -21,29 +44,205 @@ class ProductTable:
         sqlalchemy.Column("filename", sqlalchemy.String(length=100)),
     )
 
-    def __init__(self, db: Database):
-        self._db = db
+    @classmethod
+    async def ainit(cls) -> None:
+        await cls.create_if_not_exists()
 
-    async def create_if_not_exists(self) -> None:
-        schema = sqlalchemy.schema.CreateTable(self._table, if_not_exists=True)
-        await self._db.execute(str(schema.compile()))
+        if await cls.empty():
+            # TODO: read this data from CSV or something
+            # id: int32, name: string, filename: string
+            products: dict[int, dict[str, str]] = {
+                # Coffee
+                1: {"name": "ブレンドコーヒー", "filename": "/coffee01_blend.png"},
+                2: {"name": "アメリカンコーヒー", "filename": "/coffee02_american.png"},
+                3: {"name": "カフェオレコーヒー", "filename": "/coffee03_cafeole.png"},
+                4: {
+                    "name": "ブレンドブラックコーヒー",
+                    "filename": "/coffee04_blend_black.png",
+                },
+                5: {
+                    "name": "カプチーノコーヒー",
+                    "filename": "/coffee05_cappuccino.png",
+                },
+                6: {
+                    "name": "カフェラテコーヒー",
+                    "filename": "/coffee06_cafelatte.png",
+                },
+                7: {
+                    "name": "マキアートコーヒー",
+                    "filename": "/coffee07_cafe_macchiato.png",
+                },
+                8: {"name": "モカコーヒー", "filename": "/coffee08_cafe_mocha.png"},
+                9: {
+                    "name": "カラメルコーヒー",
+                    "filename": "/coffee09_caramel_macchiato.png",
+                },
+                10: {"name": "アイスコーヒー", "filename": "/coffee10_iced_coffee.png"},
+                11: {
+                    "name": "アイスミルクコーヒー",
+                    "filename": "/coffee11_iced_milk_coffee.png",
+                },
+                12: {
+                    "name": "エスプレッソコーヒー",
+                    "filename": "/coffee12_espresso.png",
+                },
+                # Tea
+                13: {"name": "レモンティー", "filename": "/tea_lemon.png"},
+                14: {"name": "ミルクティー", "filename": "/tea_milk.png"},
+                15: {"name": "ストレイトティー", "filename": "/tea_straight.png"},
+                # Others
+                16: {"name": "シュガー", "filename": "/cooking_sugar_stick.png"},
+                17: {"name": "ミルクシロップ", "filename": "/sweets_milk_cream.png"},
+            }
+            await cls.insert_many(
+                [
+                    Product(**{"product_id": product_id, **d})
+                    for product_id, d in products.items()
+                ],
+            )
 
-    async def select_all(self) -> list[Product]:
-        return [
-            Product(row["product_id"], row["name"], row["filename"])
-            for row in await self._db.fetch_all(self._table.select())
-        ]
+    @classmethod
+    async def select_all(cls) -> list[Product]:
+        return [Product(**dict(row)) for row in await db.fetch_all(cls.TABLE.select())]
 
-    async def empty(self) -> bool:
-        return await self._db.fetch_one(self._table.select()) is None
-
-    async def by_product_id(self, product_id: int) -> Product | None:
-        query = self._table.select().where(self._table.c.product_id == product_id)
+    @classmethod
+    async def by_product_id(cls, product_id: int) -> Product | None:
+        query = cls.TABLE.select().where(cls.TABLE.c.product_id == product_id)
         return (
-            None
-            if (row := await self._db.fetch_one(query)) is None
-            else Product(row["product_id"], row["name"], row["filename"])
+            None if (row := await db.fetch_one(query)) is None else Product(**dict(row))
         )
 
-    async def insert_many(self, products: list[Product]) -> None:
-        await self._db.execute_many(self._table.insert(), products)
+    @classmethod
+    async def insert_many(cls, products: list[Product]) -> None:
+        await db.execute_many(
+            cls.TABLE.insert(),
+            [p.model_dump() for p in products],
+        )
+
+
+class PlacedOrder(BaseModel):
+    placement_id: int
+    item_no: int
+    product_id: int
+
+
+class PlacedOrderTable(Table):
+    TABLE = sqlalchemy.Table(
+        "placed_orders",
+        sqlalchemy.MetaData(),
+        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+        sqlalchemy.Column("placement_id", sqlalchemy.Integer),
+        sqlalchemy.Column("item_no", sqlalchemy.Integer),
+        sqlalchemy.Column("product_id", sqlalchemy.Integer),
+    )
+
+    last_placement_id: int | None
+
+    @classmethod
+    async def ainit(cls) -> None:
+        await cls.create_if_not_exists()
+        await cls.update_last_placement_id()
+
+    @classmethod
+    async def update_last_placement_id(cls) -> None:
+        cls.last_placement_id = await db.fetch_val(
+            sqlalchemy.sql.expression.func.max(cls.TABLE.c.placement_id).select()
+        )
+
+    @classmethod
+    async def select_all(cls) -> list[PlacedOrder]:
+        return [
+            PlacedOrder(**dict(row)) for row in await db.fetch_all(cls.TABLE.select())
+        ]
+
+    @classmethod
+    async def by_placement_id(cls, placement_id: int) -> list[PlacedOrder]:
+        query = cls.TABLE.select().where(cls.TABLE.c.placement_id == placement_id)
+        return [PlacedOrder(**dict(row)) for row in await db.fetch_all(query)]
+
+    @classmethod
+    async def issue(cls, product_ids: list[int]) -> int:
+        placement_id = (cls.last_placement_id or 0) + 1
+        await db.execute_many(
+            cls.TABLE.insert().values(placement_id=placement_id),
+            [
+                {"item_no": idx, "product_id": product_id}
+                for idx, product_id in enumerate(product_ids)
+            ],
+        )
+        cls.last_placement_id = placement_id
+        return placement_id
+
+    # NOTE: this function needs authorization since it destroys all receipts
+    #
+    @classmethod
+    async def clear(cls) -> None:
+        await db.execute(cls.TABLE.delete())
+        await cls.update_last_placement_id()
+
+
+class PlacementStatus(BaseModel):
+    placement_id: int
+    canceled: bool
+    done: bool
+
+
+class PlacementStatusTable(Table):
+    TABLE = sqlalchemy.Table(
+        "placements",
+        sqlalchemy.MetaData(),
+        sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+        sqlalchemy.Column("placement_id", sqlalchemy.Integer),
+        sqlalchemy.Column(
+            "canceled", sqlalchemy.Boolean, server_default=sqlalchemy.text("0")
+        ),
+        sqlalchemy.Column(
+            "done", sqlalchemy.Boolean, server_default=sqlalchemy.text("0")
+        ),
+    )
+
+    @classmethod
+    async def ainit(cls) -> None:
+        await cls.create_if_not_exists()
+
+    @classmethod
+    async def insert(cls, placement_id: int):
+        await db.execute(cls.TABLE.insert(), {"placement_id": placement_id})
+
+    @classmethod
+    async def update(cls, placement_id: int, canceled: bool, done: bool):
+        query = cls.TABLE.update().where(cls.TABLE.c.placement_id == placement_id)
+        await db.execute(query, {"canceled": canceled, "done": done})
+
+    @classmethod
+    async def cancel(cls, placement_id: int):
+        await cls.update(placement_id, canceled=True, done=False)
+
+    @classmethod
+    async def done(cls, placement_id: int):
+        await cls.update(placement_id, canceled=False, done=True)
+
+    @classmethod
+    async def reset(cls, placement_id: int):
+        await cls.update(placement_id, canceled=False, done=False)
+
+    @classmethod
+    async def select_all(cls) -> list[PlacementStatus]:
+        return [
+            PlacementStatus(**dict(row))
+            for row in await db.fetch_all(cls.TABLE.select())
+        ]
+
+
+async def _startup_db():
+    await db.connect()
+    await ProductTable.ainit()
+    await PlacedOrderTable.ainit()
+    await PlacementStatusTable.ainit()
+
+
+async def _shutdown_db():
+    await db.disconnect()
+
+
+startup_and_shutdown_db = (_startup_db, _shutdown_db)
--- app/main.py
+++ app/main.py
@@ -1,76 +1,30 @@
 import os
 from contextlib import asynccontextmanager
 
-from databases import Database
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from .db import Product, ProductTable
-
-DATABASE_URL = "sqlite:///app.db"
-database = Database(DATABASE_URL)
+from .db import (
+    PlacedOrderTable,
+    PlacementStatusTable,
+    Product,
+    ProductTable,
+    startup_and_shutdown_db,
+)
 
 DEBUG = True if os.environ.get("ORDER_SYSTEM_DEBUG") else False
-
-product_table = ProductTable(database)
 
 
 # https://stackoverflow.com/a/65270864
 # https://fastapi.tiangolo.com/advanced/events/
 @asynccontextmanager
 async def lifespan(_: FastAPI):
-    await database.connect()
-
-    await product_table.create_if_not_exists()
-    if await product_table.empty():
-        # TODO: read this data from csv or something
-        # id: int32, name: string, filename: string
-        products: dict[int, dict[str, str]] = {
-            # Coffee
-            1: {"name": "ブレンドコーヒー", "filename": "/coffee01_blend.png"},
-            2: {"name": "アメリカンコーヒー", "filename": "/coffee02_american.png"},
-            3: {"name": "カフェオレコーヒー", "filename": "/coffee03_cafeole.png"},
-            4: {
-                "name": "ブレンドブラックコーヒー",
-                "filename": "/coffee04_blend_black.png",
-            },
-            5: {"name": "カプチーノコーヒー", "filename": "/coffee05_cappuccino.png"},
-            6: {"name": "カフェラテコーヒー", "filename": "/coffee06_cafelatte.png"},
-            7: {
-                "name": "マキアートコーヒー",
-                "filename": "/coffee07_cafe_macchiato.png",
-            },
-            8: {"name": "モカコーヒー", "filename": "/coffee08_cafe_mocha.png"},
-            9: {
-                "name": "カラメルコーヒー",
-                "filename": "/coffee09_caramel_macchiato.png",
-            },
-            10: {"name": "アイスコーヒー", "filename": "/coffee10_iced_coffee.png"},
-            11: {
-                "name": "アイスミルクコーヒー",
-                "filename": "/coffee11_iced_milk_coffee.png",
-            },
-            12: {"name": "エスプレッソコーヒー", "filename": "/coffee12_espresso.png"},
-            # Tea
-            13: {"name": "レモンティー", "filename": "/tea_lemon.png"},
-            14: {"name": "ミルクティー", "filename": "/tea_milk.png"},
-            15: {"name": "ストレイトティー", "filename": "/tea_straight.png"},
-            # Others
-            16: {"name": "シュガー", "filename": "/cooking_sugar_stick.png"},
-            17: {"name": "ミルクシロップ", "filename": "/sweets_milk_cream.png"},
-        }
-        await product_table.insert_many(
-            [
-                Product(product_id, d["name"], d["filename"])
-                for product_id, d in products.items()
-            ],
-        )
-
+    startup_db, shutdown_db = startup_and_shutdown_db
+    await startup_db()
     yield
-
-    await database.disconnect()
+    await shutdown_db()
 
 
 app = FastAPI(debug=DEBUG, lifespan=lifespan)
@@ -92,8 +46,10 @@ async def get_root(request: Request):
     )
 
 
-# NOTE: I am not sure if these data should be stored in database or not.
-item_list_sessions: dict[int, list[Product | None]] = {}
+# NOTE: Do NOT store this data in database (the data is transient and should be kept in memory)
+# NOTE: Or should this be optionally stored in database?
+order_sessions: dict[int, list[Product | None]] = {}
+last_session_id = 0
 
 
 @app.get("/order", response_class=RedirectResponse)
@@ -103,56 +59,208 @@ async def get_order_redirect():
 
 @app.get("/order/new", response_class=RedirectResponse)
 async def get_order_new():
-    new_session_id = len(item_list_sessions.keys())
-    item_list_sessions[new_session_id] = []
+    global last_session_id
+    last_session_id += 1
+    new_session_id = last_session_id
+    order_sessions[new_session_id] = []
     return RedirectResponse(f"/order/{new_session_id}")
 
 
 @app.get("/order/{session_id}", response_class=HTMLResponse)
-async def get_order(request: Request, session_id: int):
-    item_list = item_list_sessions.get(session_id)
-    if item_list is None:
+async def get_order_session(request: Request, session_id: int):
+    order_items = order_sessions.get(session_id)
+    if order_items is None:
         raise HTTPException(status_code=404, detail=f"Session {session_id} not found")
 
     idx_item_pairs = [
-        (idx, item) for idx, item in enumerate(item_list) if item is not None
+        (idx, item) for idx, item in enumerate(order_items) if item is not None
     ]
     return templates.TemplateResponse(
         "order.html",
         {
             "request": request,
-            "products": await product_table.select_all(),
+            "products": await ProductTable.select_all(),
             "session_id": session_id,
             "idx_item_pairs": idx_item_pairs,
         },
     )
 
 
-@app.post("/order/{session_id}/item", response_class=HTMLResponse)
-async def post_order_item(request: Request, session_id: int, product_id: int):
-    if (product := await product_table.by_product_id(product_id)) is None:
-        raise HTTPException(status_code=404, detail=f"Product {product_id} not found")
-    if (item_list := item_list_sessions.get(session_id)) is None:
+@app.post("/order/{session_id}", response_class=HTMLResponse)
+async def place_order(request: Request, session_id: int):
+    if (order_items := order_sessions.get(session_id)) is None:
         raise HTTPException(status_code=404, detail=f"Session {session_id} not found")
-    index = len(item_list)
-    item_list.append(product)
+
+    idx_item_pairs = [
+        (idx, item) for idx, item in enumerate(order_items) if item is not None
+    ]
+
+    if len(idx_item_pairs) == 0:
+        return templates.TemplateResponse(
+            "components/order-session.html",
+            {
+                "request": request,
+                "session_id": session_id,
+                "idx_item_pairs": [],
+                "placement_status": "Error: there is no item",
+            },
+        )
+
+    order_sessions.pop(session_id)
+
+    placement_id = await PlacedOrderTable.issue(
+        [item.product_id for item in order_items if item is not None]
+    )
+    # TODO: add a branch for out of stock error
+    await PlacementStatusTable.insert(placement_id)
+    placement_status = f"発行注文番号: {placement_id}"
     return templates.TemplateResponse(
-        "components/item.html",
+        "components/order-session.html",
         {
             "request": request,
-            "product": product,
             "session_id": session_id,
-            "index": index,
+            "idx_item_pairs": idx_item_pairs,
+            "placement_status": placement_status,
+            "order_frozen": True,
+        },
+    )
+
+
+@app.post("/order/{session_id}/item")
+async def add_order_item(
+    request: Request, session_id: int, product_id: int
+) -> Response:
+    if (product := await ProductTable.by_product_id(product_id)) is None:
+        raise HTTPException(status_code=404, detail=f"Product {product_id} not found")
+    if (order_items := order_sessions.get(session_id)) is None:
+        # NOTE: the branching below is a bit complicated so it might be changed in the future
+        if request.headers["HX-Request"] == "true":
+            # If it is a request from `hx-post`, respond with a new order session even when the `session_id` is not valid
+            global last_session_id
+            await get_order_new()
+            new_session_id = last_session_id
+            await add_order_item(request, new_session_id, product_id)
+            return Response(
+                f"Session {session_id} not found",
+                status_code=404,
+                headers={"hx-redirect": f"/order/{new_session_id}"},
+            )
+        else:
+            # otherwise report back that the `session_id` is not valid
+            raise HTTPException(
+                status_code=404, detail=f"Session {session_id} not found"
+            )
+
+    order_items.append(product)
+    idx_item_pairs = [
+        (idx, item) for idx, item in enumerate(order_items) if item is not None
+    ]
+    return templates.TemplateResponse(
+        "components/order-session.html",
+        {
+            "request": request,
+            "session_id": session_id,
+            "idx_item_pairs": idx_item_pairs,
         },
     )
 
 
 @app.delete("/order/{session_id}/item/{index}", response_class=HTMLResponse)
 async def delete_order_item(session_id: int, index: int):
-    item_list = item_list_sessions.get(session_id)
-    if item_list is None:
+    order_items = order_sessions.get(session_id)
+    if order_items is None:
         raise HTTPException(status_code=404, detail=f"Session {session_id} not found")
-    if item_list[index] is None:
-        raise HTTPException(status_code=404, detail=f"List item {index} not found")
+    if order_items[index] is None:
+        raise HTTPException(status_code=404, detail=f"Order item {index} not found")
     else:
-        item_list[index] = None
+        order_items[index] = None
+    return
+
+
+@app.post("/order/{session_id}/clear")
+async def clear_order_items(request: Request, session_id: int) -> Response:
+    if (order_items := order_sessions.get(session_id)) is None:
+        # NOTE: the branching below is a bit complicated so it might be changed in the future
+        if request.headers.get("HX-Request") == "true":
+            # If it is a request from `hx-post`, respond with a new order session even when the `session_id` is not valid
+            global last_session_id
+            await get_order_new()
+            new_session_id = last_session_id
+            return Response(
+                f"Session {session_id} not found",
+                status_code=404,
+                headers={"hx-redirect": f"/order/{new_session_id}"},
+            )
+        else:
+            # otherwise report back that the `session_id` is not valid
+            raise HTTPException(
+                status_code=404, detail=f"Session {session_id} not found"
+            )
+
+    order_items.clear()
+
+    return templates.TemplateResponse(
+        "components/order-session.html",
+        {
+            "request": request,
+            "session_id": session_id,
+            "idx_item_pairs": [],
+        },
+    )
+
+
+# TODO: add proper path operation for order deferral
+# # TODO: Store this data in database
+# deferred_order_sessions: dict[int, list[Product | None]] = {}
+#
+#
+# @app.post("/order/{session_id}/defer", response_class=HTMLResponse)
+# async def post_order_defer_session(request: Request, session_id: int):
+#     if (order_items := order_sessions.get(session_id)) is None:
+#         raise HTTPException(status_code=404, detail=f"Session {session_id} not found")
+#     if deferred_order_sessions.get(session_id):
+#         raise HTTPException(
+#             status_code=status.HTTP_409_CONFLICT,
+#             detail=f"Deferred session {session_id} already exists",
+#         )
+#     order_sessions.pop(session_id)
+#     deferred_order_sessions[session_id] = order_items
+#     # TODO: respond with a message about the success of the deferral action
+#     # return templates.TemplateResponse(
+#     #     "components/item.html",
+#     #     {
+#     #         "request": request,
+#     #         "session_id": session_id,
+#     #     },
+#     # )
+
+
+# TODO: only expose this path operation when in debug mode
+@app.get("/test")
+async def test():
+    return {
+        "product_table": await ProductTable.select_all(),
+        "order_sessions": order_sessions,
+        "placed_order_table": await PlacedOrderTable.select_all(),
+        "placement_status_table": await PlacementStatusTable.select_all(),
+    }
+
+
+# TODO: only expose this path operation when in debug mode
+@app.get("/test/placed_order_sessions/{placement_id}", response_class=HTMLResponse)
+async def test_placed_order_sessions(request: Request, placement_id: int):
+    order_items = await PlacedOrderTable.by_placement_id(placement_id)
+    # NOTE: this is a performance nightmare; should use a proper SQl stmt
+    idx_item_pairs = [
+        (idx, await ProductTable.by_product_id(item.product_id))
+        for idx, item in enumerate(order_items)
+        if item is not None
+    ]
+    return templates.TemplateResponse(
+        "components/order-session.html",
+        {
+            "request": request,
+            "session_id": placement_id,
+            "idx_item_pairs": idx_item_pairs,
+        },
+    )
