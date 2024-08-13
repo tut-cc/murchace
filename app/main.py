7dbb355da1e68384c2a5ce0f72f9fcad10dd5c01
exflikt <qff3P.m8d1ue@protonmail.com>
2024-08-14 01:24:20 +0900

    Change the name of path /order to /orders

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from .db import (
     startup_and_shutdown_db,
 )
 from .env import DEBUG
-from .routers import order, placements
+from .routers import orders, placements
 from .templates import templates
 
 
@@ -29,7 +29,7 @@ app = FastAPI(debug=DEBUG, lifespan=lifespan)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(order.router)
+app.include_router(orders.router)
 app.include_router(placements.router)
 
 
@@ -44,25 +44,7 @@ if DEBUG:
     async def test():
         return {
             "product_table": await ProductTable.select_all(),
-            "order_sessions": order.order_sessions,
+            "order_sessions": orders.order_sessions,
             "placed_order_table": await PlacedOrderTable.select_all(),
             "placement_status_table": await PlacementStatusTable.select_all(),
         }
-
-    @app.get("/test/placed_order_sessions/{placement_id}", response_class=HTMLResponse)
-    async def test_placed_order_sessions(request: Request, placement_id: int):
-        order_items = await PlacedOrderTable.by_placement_id(placement_id)
-        # NOTE: this is a performance nightmare; should use a proper SQL stmt
-        idx_item_pairs = [
-            (idx, await ProductTable.by_product_id(item.product_id))
-            for idx, item in enumerate(order_items)
-            if item is not None
-        ]
-        return templates.TemplateResponse(
-            "components/order-session.html",
-            {
-                "request": request,
-                "session_id": placement_id,
-                "idx_item_pairs": idx_item_pairs,
-            },
-        )
