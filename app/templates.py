fb41d974bebb2814ee888e05000d9869777566bd
exflikt <qff3P.m8d1ue@protonmail.com>
2024-08-18 19:08:12 +0900

    Fix layout.html to change styles.css and htmx.js files based on debug/prod mode

--- app/templates.py
+++ app/templates.py
@@ -1,10 +1,21 @@
+from typing import Any
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
 from .env import DEBUG
 
+
+def debug_context(_: Request) -> dict[str, Any]:
+    return {"DEBUG": DEBUG}
+
+
 if DEBUG:
     templates = Jinja2Templates(
-        directory="app/templates", extensions=["jinja2.ext.debug"]
+        directory="app/templates",
+        context_processors=[debug_context],
+        extensions=["jinja2.ext.debug"],
     )
 else:
-    templates = Jinja2Templates(directory="app/templates")
+    templates = Jinja2Templates(
+        directory="app/templates", context_processors=[debug_context]
+    )
