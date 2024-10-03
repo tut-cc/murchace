8ebf98e47b3cbf3333fd5a2f9526fa48e90d6a65
DDlia <gh@ddlia.com>
2024-10-03 18:10:27 +0900

    Implemented statistics page

--- app/routers/stat.py
+++ app/routers/stat.py
@@ -2,15 +2,25 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
 from .. import templates
+from ..store import (
+    PlacedItemTable,
+    ProductTable,
+    PlacementTable,
+)
 
 import sqlite3
 import csv
 import os
 
+from datetime import datetime
+from typing import Tuple, List, Dict, Any
+import statistics
+
 router = APIRouter()
 
 DATABASE_URL = os.path.abspath("./db/app.db")
 CSV_OUTPUT_PATH = os.path.abspath("./static/stat.csv")
+
 
 def write_csv():
     conn = sqlite3.connect(DATABASE_URL)
@@ -55,9 +65,100 @@ def write_csv():
     conn.close()
 
 
+async def compute_total_sales() -> Tuple[int, int, List[Dict[str, Any]]]:
+    product_table = await ProductTable.select_all()
+    placed_item_table = await PlacedItemTable.select_all()
+    placement_table = await PlacementTable.select_all()
+
+    product_price_map = {product.product_id: product for product in product_table}
+    total_sales_all_time = 0
+    total_sales_today = 0
+    sales_summary = []
+
+    today = datetime.utcnow().date()
+    print(today)
+
+    for item in placed_item_table:
+        product_id = item.product_id
+        placement = next(
+            (p for p in placement_table if p.placement_id == item.placement_id), None
+        )
+
+        if (
+            placement
+            and placement.canceled_at is None
+            and product_id in product_price_map
+        ):
+            product_info = product_price_map[product_id]
+
+            sales_summary.append(
+                {
+                    "name": product_info.name,
+                    "filename": product_info.filename,
+                    "count": 1,
+                    "total_sales": product_info.price,
+                    "no_stock": product_info.no_stock,
+                }
+            )
+
+            total_sales_all_time += product_info.price
+
+            placed_date = placement.placed_at.date()
+            if placed_date == today:
+                total_sales_today += product_info.price
+
+    sales_summary_aggregated = {}
+    for sale in sales_summary:
+        product_name = sale["name"]
+        if product_name not in sales_summary_aggregated:
+            sales_summary_aggregated[product_name] = sale
+        else:
+            sales_summary_aggregated[product_name]["count"] += 1
+            sales_summary_aggregated[product_name]["total_sales"] += sale["total_sales"]
+
+    sales_summary_list = list(sales_summary_aggregated.values())
+
+    return total_sales_all_time, total_sales_today, sales_summary_list
+
+
+async def compute_average_service_time() -> float:
+    placement_table = await PlacementTable.select_all()
+
+    service_times = []
+
+    for placement in placement_table:
+        if placement.completed_at is not None:
+            placed_at = datetime.fromisoformat(str(placement.placed_at))
+            completed_at = datetime.fromisoformat(str(placement.completed_at))
+
+            time_diff = (completed_at - placed_at).total_seconds()
+            service_times.append(time_diff)
+
+    if service_times:
+        average_service_time = statistics.mean(service_times) / 60
+    else:
+        average_service_time = 0
+
+    return round(average_service_time, 4)
+
+
 @router.get("/stat", response_class=HTMLResponse)
 async def get_stat(request: Request):
-    return HTMLResponse(templates.stat(request))
+    (
+        total_sales_all_time,
+        total_sales_today,
+        sales_summary_list,
+    ) = await compute_total_sales()
+    average_service_time = await compute_average_service_time()
+    return HTMLResponse(
+        templates.stat(
+            request,
+            total_sales_all_time,
+            total_sales_today,
+            sales_summary_list,
+            average_service_time,
+        )
+    )
 
 
 @router.post("/stat/generate-csv", response_class=HTMLResponse)
