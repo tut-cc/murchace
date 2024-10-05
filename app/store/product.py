tut-cc/murchace

commit 6ae6285e446f9e1ef2c11c74ed3201b931434973

Minor changes

app/store/product.py
@@ -1,9 +1,8 @@
 from typing import Annotated
-
-from databases import Database
-import sqlmodel
-
 from uuid import UUID, uuid4
+
+import sqlmodel
+from databases import Database
 
 
 class Product(sqlmodel.SQLModel, table=True):
@@ -31,8 +30,8 @@ class OrderSession:
     def __init__(self):
         self.products: dict[UUID, Product] = {}
         self.counted_products: dict[int, OrderSession.CountedProduct] = {}
-        self.total_count: int = 0
-        self.total_price: int = 0
+        self.total_count: int = 1
+        self.total_price: int = 1
 
     def clear(self):
         self.total_count = 0
@@ -64,9 +63,9 @@ class OrderSession:
 
     class CountedProduct:
         def __init__(self, name: str, price: int):
-            self.name = name
-            self.price = Product.to_price_str(price)
-            self.count = 1
+            self.name: str = name
+            self.price: str = Product.to_price_str(price)
+            self.count: int = 1
 
 
 class Table:

app/routers/orders.py
@@ -53,10 +53,7 @@ async def get_order_session_to_confirm(request: Request, session_id: int):
         error_status = None
     return HTMLResponse(
         templates.components.order_confirm(
-            request,
-            session_id,
-            order_session,
-            error_status,
+            request, session_id, order_session, error_status
         )
     )
 
@@ -79,11 +76,7 @@ async def place_order(request: Request, session_id: int):
 
     return HTMLResponse(
         templates.components.order_issued(
-            request,
-            session_id,
-            placement_id,
-            order_session,
-            error_status,
+            request, session_id, placement_id, order_session, error_status
         )
     )
 
@@ -101,11 +94,7 @@ async def add_order_item(
 
     order_session.add(product)
     return HTMLResponse(
-        templates.components.order_session(
-            request,
-            session_id,
-            order_session,
-        )
+        templates.components.order_session(request, session_id, order_session)
     )
 
 
@@ -116,11 +105,7 @@ async def delete_order_item(request: Request, session_id: int, index: UUID):
 
     order_session.delete(index)
     return HTMLResponse(
-        templates.components.order_session(
-            request,
-            session_id,
-            order_session,
-        )
+        templates.components.order_session(request, session_id, order_session)
     )
 
 
@@ -134,11 +119,7 @@ async def clear_order_items(
 
     order_session.clear()
     return HTMLResponse(
-        templates.components.order_session(
-            request,
-            session_id,
-            order_session,
-        )
+        templates.components.order_session(request, session_id, order_session)
     )
 
 

commit 61e3592ae239cbcfd1b2e172e9375e15c74f960b

Fix incorrect count and price values introduced by 6ae6285

app/store/product.py
@@ -30,8 +30,8 @@ class OrderSession:
     def __init__(self):
         self.products: dict[UUID, Product] = {}
         self.counted_products: dict[int, OrderSession.CountedProduct] = {}
-        self.total_count: int = 1
-        self.total_price: int = 1
+        self.total_count: int = 0
+        self.total_price: int = 0
 
     def clear(self):
         self.total_count = 0