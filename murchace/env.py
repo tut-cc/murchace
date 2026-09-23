71c688e0b44da5913b8229df4211196959eb3a99
DDlia <gh@ddlia.com>
2026-09-24 00:16:23 +0900

    Add environment variables for receipt printer

--- murchace/env.py
+++ murchace/env.py
@@ -1,3 +1,6 @@
 import os
 
 DEBUG = bool(os.environ.get("MURCHACE_DEBUG"))
+RECEIPT_PRINTER_HOST = os.environ.get("MURCHACE_RECEIPT_PRINTER_HOST", "")
+RECEIPT_PRINTER_PORT = int(os.environ.get("MURCHACE_RECEIPT_PRINTER_PORT", "9100"))
+RECEIPT_STORE_NAME = os.environ.get("MURCHACE_RECEIPT_STORE_NAME", "murchace")
